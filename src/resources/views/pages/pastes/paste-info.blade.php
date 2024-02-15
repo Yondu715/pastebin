5eb698c02c20a282c61e9f068beeff15df2ce79a
Yondu715 <maxv153218@gmail.com>
2024-02-15 13:32:09 +0700

    feat: add view for paste info

--- src/resources/views/pages/pastes/paste-info.blade.php
+++ src/resources/views/pages/pastes/paste-info.blade.php
@@ -4,8 +4,6 @@
 @endsection
 @section('content')
     <div class="container d-flex flex-column gap-5 mt-2 mb-2">
-        @foreach ($publicPastes as $paste)
-            <x-paste-card :paste="$paste"/>
-        @endforeach
+        <x-paste-card :paste="$paste" />
     </div>
 @endsection
