a3215f641bd7d852832fd73992b7d3199c5a8412
Yondu715 <maxv153218@gmail.com>
2024-02-15 13:41:04 +0700

    fix: rename redirect route after logout

--- src/app/Http/Controllers/Web/AuthController.php
+++ src/app/Http/Controllers/Web/AuthController.php
@@ -60,6 +60,6 @@ class AuthController extends Controller
         Auth::logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
-        return redirect()->route('login');
+        return redirect()->route('auth.login');
     }
 }
