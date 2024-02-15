9972f094996db7c373e48fd1487215577f339685
Yondu715 <maxv153218@gmail.com>
2024-02-15 13:26:32 +0700

    feat: add link to create complaint form in paste card view

--- src/resources/views/components/paste-card.blade.php
+++ src/resources/views/components/paste-card.blade.php
@@ -1,11 +1,21 @@
 <div class="card">
-    <div class="card-header">
-        {{ $paste->title }}
-    </div>
+    <a href="{{ route('pastes.show', ['hash' => $paste->hash]) }}"
+        class="link-offset-2 link-underline link-underline-opacity-0 text-dark">
+        <div class="card-header">
+            {{ $paste->title }}
+        </div>
+    </a>
     <div class="card-body">
         <p class="card-text">{{ $paste->text }}</p>
     </div>
-    <div class="card-footer text-muted">
+    <div class="d-flex justify-content-between card-footer text-muted">
         Автор: {{ $paste->author->name }}
+        @auth
+            <a href="{{ route('pastes.complaint.create', ['pasteId' => $paste->id]) }}">
+                Пожаловаться на пасту
+            </a>
+        @endauth
+
     </div>
+
 </div>
