4fba994a8e6587c592bb6c8f28780e916cc1d030
Yondu715 <maxv153218@gmail.com>
2024-02-16 13:40:33 +0700

    feat: add block with private pastes for auth user

--- src/resources/views/pages/pastes/home.blade.php
+++ src/resources/views/pages/pastes/home.blade.php
@@ -3,9 +3,24 @@
     <x-header />
 @endsection
 @section('content')
-    <div class="container d-flex flex-column gap-5 mt-2 mb-2">
-        @foreach ($publicPastes as $paste)
-            <x-paste-card :paste="$paste"/>
-        @endforeach
+    <div class="container d-flex flex-row gap-5 mt-2 mb-2">
+        <div class="row">
+            <span>Последние 10 паст в системе</span>
+            @foreach ($publicPastes as $paste)
+            <div class="col-md-6 mb-4">
+                <x-paste-card :paste="$paste"/>
+            </div>
+            @endforeach
+        </div>
+        @auth
+        <div class="row">
+            <span>Мои последние пасты </span>
+            @foreach ($privatePastes as $paste)
+            <div class="col-md-6 mb-4">
+                <x-paste-card :paste="$paste"/>
+            </div>
+            @endforeach
+        </div>
+        @endauth
     </div>
 @endsection
