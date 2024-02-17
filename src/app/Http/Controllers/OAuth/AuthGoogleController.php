2d8d056a3e238e255a28f98f419d7de492d550ec
Yondu715 <maxv153218@gmail.com>
2024-02-17 13:50:38 +0700

    feat: add login via google

--- src/app/Http/Controllers/OAuth/AuthGoogleController.php
+++ src/app/Http/Controllers/OAuth/AuthGoogleController.php
@@ -2,13 +2,21 @@
 
 namespace App\Http\Controllers\OAuth;
 
-use App\DTO\RegisterDto;
+use App\DTO\CreateSocialiteUserDto;
 use App\Http\Controllers\Controller;
+use App\Services\AuthService;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
 
 class AuthGoogleController extends Controller
 {
+
+    public function __construct(
+        private AuthService $authService
+    ) {
+    }
+
     public function redirectToProvider(): RedirectResponse
     {
         return Socialite::driver('google')->redirect();
@@ -17,7 +25,9 @@ class AuthGoogleController extends Controller
     public function handleProviderCallback()
     {
         $socialiteUser = Socialite::driver('google')->stateless()->user();
-        $registerUser = RegisterDto::fromSocialite($socialiteUser);
-        return response()->json([$registerUser]);
+        $createSocialiteUserDto = CreateSocialiteUserDto::fromSocialite($socialiteUser);
+        $user = $this->authService->loginViaSocial($createSocialiteUserDto, 'google');
+        Auth::login($user);
+        return redirect()->route('pastes.index');
     }
 }
